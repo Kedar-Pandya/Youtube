e7ceaa741cab8f1931906fb116e030c111032573
Kedar231299 <kedarnathpandya@gmail.com>
2020-12-31 11:31:47 +0530

    Add TableView and it's Delegate,DataSource and ModelDelegate Methods also Print the Title of Videos

--- youtube/Constants.swift
+++ youtube/Constants.swift
@@ -12,5 +12,7 @@ struct Constants{
     static var API_KEY = "AIzaSyDIJj23c6iIx_TFj4x5jKKVyzyc1tgTZtw"
     static var PLAYLIST_ID = "UUsTcErHg8oDvUnTzoqsYeNw"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+    static var VIDEOCELL_ID = "VideoCell"
 }
 
