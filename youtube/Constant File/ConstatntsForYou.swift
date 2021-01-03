fbadfeefe007e0b8ad4961e4d800eb0c57bea9f8
Kedar231299 <kedarnathpandya@gmail.com>
2021-01-03 17:07:36 +0530

    Adding 'Constants For You' file for user to easily clone the project and put their API key.

---
+++ youtube/Constant File/ConstatntsForYou.swift
@@ -0,0 +1,22 @@
+//
+//  ConstatntsForYou.swift
+//  youtube
+//
+//  Created by Kedar Pandya on 03/01/21.
+
+
+
+//MARK:- Add you own API Key 
+
+//import Foundation
+//
+//struct ConstantsForYou{
+//
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//
+//    static var VIDEOCELL_ID = "VideoCell"
+//    static var YT_EMBEDED_URL = "https://www.youtube.com/watch?v="
+//}
+
